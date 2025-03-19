c2c5b3fac907932c07e28d3be040c6ae7d6d06e4
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-03-19 09:36:38 +0700

    Style/states (#4)
    * стилизация состояний номера телефона header кнопки видео about
    
    * price filter states
    
    * стилизация состояний кнопок swiper
    
    * Cтилизация состояний кнопок купить абонимент/заполнить заявку
    
    * стилизация состояний ссылок подробнее
    
    * стилизация состояний кнопок аккордеона/кнопок фильтра faq
    
    * состояния ссылок footer
    
    * логика swiper

--- source/js/modules/slider/jury-swiper-init.js
+++ source/js/modules/slider/jury-swiper-init.js
@@ -2,23 +2,59 @@
 // import 'swiper/css';
 
 import Swiper from 'swiper';
-import {Navigation} from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
+
+const slides = document.querySelectorAll('.slider-juri__slide');
+const slidesContainer = document.querySelector('.slider-juri__swiper');
+
+const mousePreventDeffault = () => {
+  slidesContainer.addEventListener('mousedown', (evt) => {
+    evt.preventDefault();
+  });
+};
 
 const swiper = new Swiper('.slider-juri__swiper', {
-  modules:[Navigation],
+  modules: [Navigation],
   loop: true,
   slidesPerView: 1,
-  // spaceBetween: 20,
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev', },
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    init: function () {
+      updateTabIndexAttribute(this);
+    },
+    slideChange: function () {
+      updateTabIndexAttribute(this);
+      if (window.innerWidth >= 1366) {
+        mousePreventDeffault();
+      }
+    },
+  },
   breakpoints: {
     768: {
       slidesPerView: 2,
-      // spaceBetween: 40,
     },
     1366: {
       slidesPerView: 4,
-      // spaceBetween: 40,
+      simulateTouch: false,
     },
-  }});
+  },
+});
+
+// еще подумай
+slidesContainer.addEventListener('focusin', (evt) => {
+  const focusedSlide = evt.target.closest('.slider-juri__slide');
+  if (focusedSlide) {
+    const index = focusedSlide.getAttribute('data-swiper-slide-index');
+    swiper.slideTo(index);
+    // swiper.slides[swiper.activeIndex + 1].setAttribute('tabindex', '0');
+  }
+});
+
+function updateTabIndexAttribute(juriSwiper) {
+  slides.forEach((slide) => {
+    slide.setAttribute('tabindex', '0');
+  });
+}
