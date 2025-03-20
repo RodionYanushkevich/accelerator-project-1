8eb29fa79021270b9a2d11d32b4760729f00be99
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-03-20 14:16:05 +0700

    фикс логики form

--- source/js/modules/form/inputs-errors-messages-shown.js
+++ source/js/modules/form/inputs-errors-messages-shown.js
@@ -1,13 +1,10 @@
 const form = document.querySelector('.form');
 
 const inputs = form.querySelectorAll('.form__input');
-// const [nameInput, phoneNumberInput] = inputs;
-
 const errors = form.querySelectorAll('.form__error-message');
 const [nameError, phoneNumberError] = errors;
 
-const DEFFAULT_NAME_ERRORS_MESSAGES = [nameError.innerHTML
-  ,phoneNumberError.innerHTML];
+const DEFFAULT_NAME_ERRORS_MESSAGES = [nameError.innerHTML ,phoneNumberError.innerHTML];
 
 form.setAttribute('novalidate', '');
 
@@ -19,17 +16,21 @@ inputs.forEach((input, index) => {
 });
 
 form.addEventListener('submit' , (evt) => {
-  inputs.forEach((input, index) => {
-    if (input.value === '') {
+  for (let i = 0 ; i < inputs.length ;i++) {
+
+    if (inputs[i].value === '') {
       evt.preventDefault();
-      errors[index].innerHTML = 'Обязательное поле';
-      errors[index].classList.remove('form__error-message--hidden');
-    }
-    if (!input.checkValidity()) {
-      evt.preventDefault();
-      errors[index].classList.remove('form__error-message--hidden');
+      errors[i].innerHTML = 'Обязательное поле';
+      errors[i].classList.remove('form__error-message--hidden');
+      return;
     }
 
-  });
+    if (!inputs[i].checkValidity()) {
+      evt.preventDefault();
+      errors[i].classList.remove('form__error-message--hidden');
+      return;
+    }
+  }
+
 });
 
